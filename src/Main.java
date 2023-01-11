64ca76d88fc313aa9054a8ea219f15d810a35a1e
adamwise <awise.developer@gmail.com>
2023-01-11 14:28:11 -0800

    added the ArrayList.add() method, and tested in main to make sure it works

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import edu.greenriver.sdev333.Arraylist;
+import edu.greenriver.sdev333.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        List<String> friends = new Arraylist<String>();
+        System.out.println("Initial size is " + friends.size());
+
+        friends.add("Jess");
+        friends.add("adam");
+        friends.add("de");
+        friends.add("kevin");
+        friends.add("carl");
+        friends.add("alex");
+        friends.add("tony");
+        friends.add("meadow");
+        friends.add("aj");
+        friends.add("Jess");
+
+
+        System.out.println("size is now " + friends.size());
     }
 }
