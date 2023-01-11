7afa2fc711e37e61b0fa1975a7dc52d262b30d84
Kevin Stone <ruca6616@gmail.com>
2023-01-11 14:27:44 -0800

    Created private fields, constructor and add method. Test code in Main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import edu.greenriver.sdev333.ArrayList;
+import edu.greenriver.sdev333.List;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        List<String> friends = new ArrayList<String>();
+        System.out.println("Initial size is " + friends.size());
+
+
+        friends.add("Jess");
+        friends.add("Tina");
+        friends.add("Josh");
+        friends.add("a");
+        friends.add("a");
+        friends.add("a");
+        friends.add("a");
+        friends.add("a");
+        friends.add("a");
+        friends.add("a");
+        friends.add("a");
+
+        System.out.println("Size is now " + friends.size());
+
     }
 }
