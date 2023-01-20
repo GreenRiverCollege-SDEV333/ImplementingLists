8163254198a71956d3984c23a23625ddff210aba
Brandon Burgess <burgess.brandon@student.greenriver.edu>
2023-01-20 17:35:08 -0500

    Main was wrong version

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         List<String> friends = new ArrayList<String>();
 
         System.out.println("Initial size is " + friends.size());
-
+        System.out.println(" ");
         friends.add("Brandon");
         friends.add("Paris");
         friends.add("Sean");
@@ -21,26 +21,48 @@ public class Main {
         friends.add("Bob");
         friends.add("Gene");
         friends.add("Louise");
+        for (int i = 0; i < friends.size(); i++) {
+            System.out.println(friends.get(i));
+        }
+        System.out.println(" ");
+        //add friend @ index
         friends.add(7,"JEEBUS");
-
-
-
-//        for (int i = 0; i < friends.size(); i++) {
-//            System.out.println(friends.get(i));
-//        }
-
+        //works
 
         Iterator<String> itr = friends.iterator();
         while(itr.hasNext()) {
             String name = itr.next();
             System.out.println(name);
         }
+        System.out.println(" ");
 
+        //Number of friends in array
         System.out.println("Size is " + friends.size());
+        //works
 
-//        for (String name : friends) {
-//            System.out.println(name);
-//        }
+        //Find last occurrence of friend with name at index
+        System.out.println("JEEBUS is at index: " + friends.indexOf("JEEBUS"));
+        //works
+        System.out.println(" ");
+
+        //Remove friend at index
+        friends.remove(7);
+        //works
+
+        for (String name : friends) {
+            System.out.println(name);
+        }
+        //re-adding removed friend
+        friends.add(7,"JEEBUS");
+        System.out.println(" ");
+
+        //remove friend by name
+        friends.remove("Gene");
+        //works
+
+        for (String name : friends) {
+            System.out.println(name);
+        }
 
     }
 }
