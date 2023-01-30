2dc3b9197c4b2e1195541c113234d7fbb2150fd7
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-01-30 14:11:40 -0800

    add methods added

--- src/edu/greenriver/sdev333/SinglyLinkedList.java
+++ src/edu/greenriver/sdev333/SinglyLinkedList.java
@@ -22,6 +22,52 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
         head = null;
         size = 0;
     }
+
+    //helper methods
+    public void validIndex(int index){
+        if(index<0 || index>= size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public class ourCustomIterator implements Iterator<ItemType>{
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+
+        //field to keep track of position
+        private Node currentPosition;
+
+        public ourCustomIterator() {
+            currentPosition = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            //check if current.next == null
+            return currentPosition != null;
+
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws //NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public ItemType next() {
+            ItemType result = currentPosition.data;
+            currentPosition = currentPosition.next;
+
+            return result;
+        }
+    }
     /**
      * Returns the number of items in this collection.
      *
@@ -62,7 +108,8 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
      */
     @Override
     public Iterator<ItemType> iterator() {
-        return null;
+        //returns our iterator
+        return new ourCustomIterator();
     }
 
     /**
@@ -177,6 +224,9 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
      */
     @Override
     public ItemType get(int index) {
+        //check index validity
+        validIndex(index);
+
         Node current = head;
         int counter = 0;
         while (counter != index){
@@ -200,6 +250,16 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
      */
     @Override
     public void set(int index, ItemType item) {
+        validIndex(index);
+
+        Node current = head;
+        int counter = 0;
+        while (counter != index){
+            current = current.next;
+            counter++;
+        }
+        current.data = item;
+
 
     }
 
@@ -217,6 +277,26 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
      */
     @Override
     public void add(int index, ItemType item) {
+        validIndex(index);
+        Node temp = new Node();
+        Node current = head;
+        int counter= 0;
+        if(index == 0){
+            temp.data = item;
+            temp.next = head;
+            head = temp;
+        }else {
+            while (counter < index - 1) {
+                current = current.next;
+                counter++;
+            }
+
+            temp.data = item;
+            temp.next = current.next;
+
+            current.next = temp;
+        }
+        size++;
 
     }
 
@@ -230,7 +310,20 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
      */
     @Override
     public void remove(int index) {
+        validIndex(index);
+        if(index == 0){
+            head = head.next;
+        }else {
+            Node current = head;
+            int counter = 0;
+            while (counter < index - 1) {
+                current = current.next;
+                counter++;
+            }
+            current.next = current.next.next;
 
+        }
+        size--;
     }
 
     /**
@@ -272,6 +365,172 @@ public class SinglyLinkedList <ItemType> implements List<ItemType>{
      */
     @Override
     public ListIterator<ItemType> listIterator() {
-        return null;
+
+        return new OurEnhancedIterator();
+    }
+
+    private class OurEnhancedIterator implements ListIterator<ItemType>{
+
+        private Node currentPosition;
+
+        public OurEnhancedIterator(){
+            currentPosition = head;
+        }
+        /**
+         * Returns {@code true} if this list iterator has more elements when
+         * traversing the list in the forward direction. (In other words,
+         * returns {@code true} if {@link #next} would return an element rather
+         * than throwing an exception.)
+         *
+         * @return {@code true} if the list iterator has more elements when
+         * traversing the list in the forward direction
+         */
+        @Override
+        public boolean hasNext() {
+            return currentPosition != null;
+        }
+
+        /**
+         * Returns the next element in the list and advances the cursor position.
+         * This method may be called repeatedly to iterate through the list,
+         * or intermixed with calls to {@link #previous} to go back and forth.
+         * (Note that alternating calls to {@code next} and {@code previous}
+         * will return the same element repeatedly.)
+         *
+         * @return the next element in the list
+         * @throws //NoSuchElementException if the iteration has no next element
+         */
+        @Override
+        public ItemType next() {
+            return null;
+        }
+
+        /**
+         * Returns {@code true} if this list iterator has more elements when
+         * traversing the list in the reverse direction.  (In other words,
+         * returns {@code true} if {@link #previous} would return an element
+         * rather than throwing an exception.)
+         *
+         * @return {@code true} if the list iterator has more elements when
+         * traversing the list in the reverse direction
+         */
+        @Override
+        public boolean hasPrevious() {
+            return false;
+        }
+
+        /**
+         * Returns the previous element in the list and moves the cursor
+         * position backwards.  This method may be called repeatedly to
+         * iterate through the list backwards, or intermixed with calls to
+         * {@link #next} to go back and forth.  (Note that alternating calls
+         * to {@code next} and {@code previous} will return the same
+         * element repeatedly.)
+         *
+         * @return the previous element in the list
+         * @throws //NoSuchElementException if the iteration has no previous
+         *                                element
+         */
+        @Override
+        public ItemType previous() {
+            return null;
+        }
+
+        /**
+         * Returns the index of the element that would be returned by a
+         * subsequent call to {@link #next}. (Returns list size if the list
+         * iterator is at the end of the list.)
+         *
+         * @return the index of the element that would be returned by a
+         * subsequent call to {@code next}, or list size if the list
+         * iterator is at the end of the list
+         */
+        @Override
+        public int nextIndex() {
+            return 0;
+        }
+
+        /**
+         * Returns the index of the element that would be returned by a
+         * subsequent call to {@link #previous}. (Returns -1 if the list
+         * iterator is at the beginning of the list.)
+         *
+         * @return the index of the element that would be returned by a
+         * subsequent call to {@code previous}, or -1 if the list
+         * iterator is at the beginning of the list
+         */
+        @Override
+        public int previousIndex() {
+            return 0;
+        }
+
+        /**
+         * Removes from the list the last element that was returned by {@link
+         * #next} or {@link #previous} (optional operation).  This call can
+         * only be made once per call to {@code next} or {@code previous}.
+         * It can be made only if {@link #add} has not been
+         * called after the last call to {@code next} or {@code previous}.
+         *
+         * @throws UnsupportedOperationException if the {@code remove}
+         *                                       operation is not supported by this list iterator
+         * @throws IllegalStateException         if neither {@code next} nor
+         *                                       {@code previous} have been called, or {@code remove} or
+         *                                       {@code add} have been called after the last call to
+         *                                       {@code next} or {@code previous}
+         */
+        @Override
+        public void remove() {
+
+        }
+
+        /**
+         * Replaces the last element returned by {@link #next} or
+         * {@link #previous} with the specified element (optional operation).
+         * This call can be made only if neither {@link #remove} nor {@link
+         * #add} have been called after the last call to {@code next} or
+         * {@code previous}.
+         *
+         * @param itemType the element with which to replace the last element returned by
+         *                 {@code next} or {@code previous}
+         * @throws UnsupportedOperationException if the {@code set} operation
+         *                                       is not supported by this list iterator
+         * @throws ClassCastException            if the class of the specified element
+         *                                       prevents it from being added to this list
+         * @throws IllegalArgumentException      if some aspect of the specified
+         *                                       element prevents it from being added to this list
+         * @throws IllegalStateException         if neither {@code next} nor
+         *                                       {@code previous} have been called, or {@code remove} or
+         *                                       {@code add} have been called after the last call to
+         *                                       {@code next} or {@code previous}
+         */
+        @Override
+        public void set(ItemType itemType) {
+
+        }
+
+        /**
+         * Inserts the specified element into the list (optional operation).
+         * The element is inserted immediately before the element that
+         * would be returned by {@link #next}, if any, and after the element
+         * that would be returned by {@link #previous}, if any.  (If the
+         * list contains no elements, the new element becomes the sole element
+         * on the list.)  The new element is inserted before the implicit
+         * cursor: a subsequent call to {@code next} would be unaffected, and a
+         * subsequent call to {@code previous} would return the new element.
+         * (This call increases by one the value that would be returned by a
+         * call to {@code nextIndex} or {@code previousIndex}.)
+         *
+         * @param itemType the element to insert
+         * @throws UnsupportedOperationException if the {@code add} method is
+         *                                       not supported by this list iterator
+         * @throws ClassCastException            if the class of the specified element
+         *                                       prevents it from being added to this list
+         * @throws IllegalArgumentException      if some aspect of this element
+         *                                       prevents it from being added to this list
+         */
+        @Override
+        public void add(ItemType itemType) {
+
+        }
     }
 }
